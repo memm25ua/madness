50d4ff2dc1eed04b5e903495bf43e362717d261c
mada <madaniilicitano@gmail.com>
2023-02-17 11:20:53 +0100

    Initial Commit

---
+++ madness/madnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  madnessApp.swift
+//  madness
+//
+//  Created by m1 on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct madnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
